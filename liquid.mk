5afc3890968a19b0b4328c2e14641409dcc3ac90
DarkBlueEdg <44828851+DarkBlueEdg@users.noreply.github.com>
2019-09-13 19:26:11 +0300

    TryOwnMe-Edg update liquid.mk
    TryOwnMe-Edg update liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -3,17 +3,20 @@ $(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
 BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
-TARGET_GAPPS_ARCH := arm
-
-PRODUCT_GMS_CLIENTID_BASE := android-sony
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_NAME := liquid_honami
 PRODUCT_DEVICE := honami
+PRODUCT_MODEL := Xperia Z1
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
 
 export LIQUID_BUILDTYPE=EXPERIMENTAL
